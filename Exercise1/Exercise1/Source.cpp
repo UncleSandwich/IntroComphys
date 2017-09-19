721a17bbab10ed830d849b3816a11e0031408900
Hangxi Li <UncleSandwich@users.noreply.github.com>
2017-09-19 09:42:34 +0200

    add a simple program: calculate square of integer.

--- Exercise1/Exercise1/Source.cpp
+++ Exercise1/Exercise1/Source.cpp
@@ -3,8 +3,16 @@
 
 using namespace std;
 
+void Number() {
+	int i;
+	for (i = 1; i <= 10; i++) {
+		cout << "i:" << i << "\t" << "i^2:" << i*i << "\n";
+	}
+}
+
 int main() {
-	cout << "Hello World!\n";
+	cout << "Hello World!\n"; //print "Hello World"
+	Number();//the program to print square of integer
 	system("pause");
 	return 0;
 }
