eb92aa0fa8a404ed74ab1797ba4855cdd4c0bf7b
Hangxi Li <UncleSandwich@users.noreply.github.com>
2017-10-17 10:19:47 +0200

    change p to 0.59

--- Ex4/Ex4/Source.cpp
+++ Ex4/Ex4/Source.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-#define PROBILITY 0.8
+#define PROBILITY 0.59
 #define L 1001
 
 int main() {
@@ -19,107 +19,107 @@ int main() {
 	do {
 		for (int icounter = 0; icounter < L*L; icounter++)
 			lat[icounter] = drand48() < PROBILITY ? 1 : 0;
-	} while (lat[(L + 1) / 2 * L + (L + 1) / 2] == 0);
 	
-	int occupied = 0;
-	for (int i = 0; i < L*L; i++) occupied += lat[i];
-	cout << "Number of occupied sites: " << occupied << "\n";
+		int occupied = 0;
+		for (int i = 0; i < L*L; i++) occupied += lat[i];
+		cout << "Number of occupied sites: " << occupied << "\n";
 
-	//label the clusters
-	for (int i = 0; i < L; i++)
-		for (int j = 0; j < L; j++) {
-			if (lat[i*L + j] == 0) continue;
-			else if (lat[i*L + j] == 1) {
-				if (i == 0 && j == 0) {  //check whether the occupied site is on the left-top corner.
-					lat[i*L + j] = k;    //if so, assign k=2
-					M[lat[i*L + j]]++;
-				}
-				if (i == 0 && j > 0) { //check whether the occupied site is on the top row but not first.
-					if (lat[i*L + (j - 1)] == 0) {
-						k++;
-						lat[i*L + j] = k;
+		//label the clusters
+		for (int i = 0; i < L; i++)
+			for (int j = 0; j < L; j++) {
+				if (lat[i*L + j] == 0) continue;
+				else if (lat[i*L + j] == 1) {
+					if (i == 0 && j == 0) {  //check whether the occupied site is on the left-top corner.
+						lat[i*L + j] = k;    //if so, assign k=2
 						M[lat[i*L + j]]++;
 					}
-					else if (lat[i*L + (j - 1)] > 1) {
-						int k0 = lat[i*L + (j - 1)];
-						while (M[k0] < 0) k0 = -M[k0];
-						lat[i*L + j] = k0;
-						M[lat[i*L + j]]++;
-					}
-				}
-				if (i > 0 && j == 0) { //check whether the occupied site is on the left periphery
-					if (lat[(i - 1)*L + j] == 0) {
-						k++;
-						lat[i*L + j] = k;
-						M[lat[i*L + j]]++;
-					}
-					else if (lat[(i - 1)*L + j] > 1) {
-						int k0 = lat[(i - 1)*L + j];
-						while (M[k0] < 0) k0 = -M[k0];
-						lat[i*L + j] = k0;
-						M[lat[i*L + j]]++;
-					}
-				}
-				if (i > 0 && j > 0) { // for other sites in lattice
-					if (lat[(i - 1)*L + j] == 0 && lat[i*L + (j - 1)] == 0) { // if top and left sites are empty
-						k++;
-						lat[i*L + j] = k;
-						M[lat[i*L + j]]++;
-					}
-					else if (lat[(i - 1)*L + j] > 1 && lat[i*L + (j - 1)] == 0) { // if left is empty and top is occupied and belong to a cluster
-						int k0 = lat[(i - 1)*L + j];
-						while (M[k0] < 0) k0 = -M[k0];
-						lat[i*L + j] = k0;
-						M[lat[i*L + j]]++;
-					}
-					else if (lat[(i - 1)*L + j] == 0 && lat[i*L + (j - 1)] > 1) { //if top is empty and left is occupied and belong to a cluster
-						int k0 = lat[i*L + (j - 1)];
-						while (M[k0] < 0) k0 = -M[k0];
-						lat[i*L + j] = k0;
-						M[lat[i*L + j]]++;
-					}
-					else if (lat[(i - 1)*L + j] > 1 && lat[i*L + (j - 1)] > 1) { // if top and left are occupied 
-						int k1 = lat[(i - 1)*L + j], k2 = lat[i*L + (j - 1)];
-						while (M[k1] < 0) k1 = -M[k1];
-						while (M[k2] < 0) k2 = -M[k2];
-						if (k1 > k2) swap(k1, k2);
-
-						lat[i*L + j] = k1;
-						if (k1 != k2) {
-							M[lat[i*L + j]] += M[k2] + 1;
-							M[k2] = -k1;
-						}
-						else if (k1 == k2)
+					if (i == 0 && j > 0) { //check whether the occupied site is on the top row but not first.
+						if (lat[i*L + (j - 1)] == 0) {
+							k++;
+							lat[i*L + j] = k;
 							M[lat[i*L + j]]++;
+						}
+						else if (lat[i*L + (j - 1)] > 1) {
+							int k0 = lat[i*L + (j - 1)];
+							while (M[k0] < 0) k0 = -M[k0];
+							lat[i*L + j] = k0;
+							M[lat[i*L + j]]++;
+						}
 					}
+					if (i > 0 && j == 0) { //check whether the occupied site is on the left periphery
+						if (lat[(i - 1)*L + j] == 0) {
+							k++;
+							lat[i*L + j] = k;
+							M[lat[i*L + j]]++;
+						}
+						else if (lat[(i - 1)*L + j] > 1) {
+							int k0 = lat[(i - 1)*L + j];
+							while (M[k0] < 0) k0 = -M[k0];
+							lat[i*L + j] = k0;
+							M[lat[i*L + j]]++;
+						}
+					}
+					if (i > 0 && j > 0) { // for other sites in lattice
+						if (lat[(i - 1)*L + j] == 0 && lat[i*L + (j - 1)] == 0) { // if top and left sites are empty
+							k++;
+							lat[i*L + j] = k;
+							M[lat[i*L + j]]++;
+						}
+						else if (lat[(i - 1)*L + j] > 1 && lat[i*L + (j - 1)] == 0) { // if left is empty and top is occupied and belong to a cluster
+							int k0 = lat[(i - 1)*L + j];
+							while (M[k0] < 0) k0 = -M[k0];
+							lat[i*L + j] = k0;
+							M[lat[i*L + j]]++;
+						}
+						else if (lat[(i - 1)*L + j] == 0 && lat[i*L + (j - 1)] > 1) { //if top is empty and left is occupied and belong to a cluster
+							int k0 = lat[i*L + (j - 1)];
+							while (M[k0] < 0) k0 = -M[k0];
+							lat[i*L + j] = k0;
+							M[lat[i*L + j]]++;
+						}
+						else if (lat[(i - 1)*L + j] > 1 && lat[i*L + (j - 1)] > 1) { // if top and left are occupied 
+							int k1 = lat[(i - 1)*L + j], k2 = lat[i*L + (j - 1)];
+							while (M[k1] < 0) k1 = -M[k1];
+							while (M[k2] < 0) k2 = -M[k2];
+							if (k1 > k2) swap(k1, k2);
+
+							lat[i*L + j] = k1;
+							if (k1 != k2) {
+								M[lat[i*L + j]] += M[k2] + 1;
+								M[k2] = -k1;
+							}
+							else if (k1 == k2)
+								M[lat[i*L + j]]++;
+						}
+					}
+
+
 				}
+			}
 
-
+		//find out the largest cluster, and erase others
+		const int k_max = k;
+		int k_largest_cluster = 2;
+		int max_M = 0;
+		for (int k = 2; k <= k_max; k++) {
+			if (M[k] < 0) continue;
+			if (M[k] > max_M) {
+				max_M = M[k] > max_M ? M[k] : max_M;
+				k_largest_cluster = k;
 			}
 		}
-
-	//find out the largest cluster, and erase others
-	const int k_max = k;
-	int k_largest_cluster = 2;
-	int max_M = 0;
-	for (int k = 2; k <= k_max; k++) {
-		if (M[k] < 0) continue;
-		if (M[k] > max_M) {
-			max_M = M[k] > max_M ? M[k] : max_M;
-			k_largest_cluster = k;
-		}
-	}
-	for (int i = 0; i < L; i++)
-		for (int j = 0; j < L; j++) {
-			if (lat[i*L + j] == k_largest_cluster) lat[i*L + j] = 1;
-			else {
-				int k0 = lat[i*L + j];
-				while (M[k0] < 0) k0 = -M[k0];
-				if (k0 == k_largest_cluster) lat[i*L + j] = 1;
-				else lat[i*L + j] = 0;
+		for (int i = 0; i < L; i++)
+			for (int j = 0; j < L; j++) {
+				if (lat[i*L + j] == k_largest_cluster) lat[i*L + j] = 1;
+				else {
+					int k0 = lat[i*L + j];
+					while (M[k0] < 0) k0 = -M[k0];
+					if (k0 == k_largest_cluster) lat[i*L + j] = 1;
+					else lat[i*L + j] = 0;
+				}
 			}
-		}
-	cout << "max_M: " << max_M << "\n";
+		cout << "max_M: " << max_M << "\n";
+	} while (lat[L / 2 * L + L / 2] == 0);
 
 
 	//cout << "Lattice clusters number\n";
@@ -131,7 +131,7 @@ int main() {
 	//}
 
 	//Sandbox Method to calculate the fractal dimension
-	if (lat[(L + 1) / 2 * L + (L + 1) / 2] == 0) cout << "the center of lattice is not occupied, please rerun the program.\a";
+	if (lat[L / 2 * L + L / 2] == 0) cout << "the center of lattice is not occupied, please rerun the program.\a";
 	else
 	{
 		int N[L+1]{ 0 }; //array to store the mass of each sandbox
