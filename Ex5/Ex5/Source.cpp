2ec2ee1c9b7d38d906f8a1d11a36d1eb9d36bbf4
Hangxi Li <UncleSandwich@users.noreply.github.com>
2017-10-26 11:15:11 +0200

    optimize ex5 to include change of particle number

--- Ex5/Ex5/Source.cpp
+++ Ex5/Ex5/Source.cpp
@@ -10,8 +10,15 @@
 
 #define CUBE_LENGTH 3001  //the size of cube containing particles
 #define RADIUS 1  //radius of the sphere particle
-#define PARTICLE_NUMBER 100  //number of particles
-#define NUMBER_OF_NUMBER_OF_SAMPLE 100
+
+#define change_smapling_number false
+#define PARTICLE_NUMBER 50  //number of particles
+#define NUMBER_OF_NUMBER_OF_SAMPLE 1000
+
+#define change_particle_number true
+#define NUMBER_OF_PARTICLE_NUMBER 1000  //number of particles
+#define NUMBER_OF_SAMPLE 100
+
 
 using namespace std;
 
@@ -30,65 +37,153 @@ double distance(particle p1, particle p2) {
 
 int main() {
 	double volume = 4. / 3. * M_PI * pow(RADIUS, 3);
-	particle *p;
-	p = new particle[PARTICLE_NUMBER];
+	
 	bool is_accepted = true;
 	//Monte Carlo
-	string str = "len=" + to_string(CUBE_LENGTH) + " r=" + to_string(RADIUS) + " n=" + to_string(PARTICLE_NUMBER) + ".txt";
-	fstream myfile(str, ios::out | ios::trunc);
-	int number_of_sample[NUMBER_OF_NUMBER_OF_SAMPLE];
-	for (int i = 0; i < NUMBER_OF_NUMBER_OF_SAMPLE; i++) {
-		number_of_sample[i] =  10 * (i + 2);
-	}
-	for (int t = 0; t < NUMBER_OF_NUMBER_OF_SAMPLE; t++) {
-		double d_mean_mean = 0;
-		for (int k = 0; k < number_of_sample[t]; k++) {
-			//generate a configration
-			for (int i = 0; i < PARTICLE_NUMBER; i++) {
-				int n = 0;
-				do {
-					n++;
-					srand48(time(NULL));
-					particle p_new{ (CUBE_LENGTH - 1) * drand48(), (CUBE_LENGTH - 1) * drand48(), (CUBE_LENGTH - 1) * drand48() };
-					//cout << "p_new.x: " << p_new.x << " p_new.y: " << p_new.y << " p_new.z: " << p_new.z << "\n";
-					is_accepted = true;
-					for (int j = 0; j < i; j++) {
-						if (distance(p_new, p[j]) < 2 * RADIUS) {
-							is_accepted = false;
-							break;
-						}
-					}
-					if (is_accepted) {
-						p[i] = p_new;
-					}
-					if (n > 100) {
-						cout << "Cant place more particle\n";
-						volume *= PARTICLE_NUMBER;
-						cout << "volume: " << volume << "\n";
-						cout << "volume fraction: " << volume / pow(CUBE_LENGTH, 3) << "\n";
-						system("pause");
-						return 0;
-					}
-				} while (!is_accepted);
-			}
-			//calculate the mean of distance
-			double d_mean = 0;
-			for (int i = 0; i < PARTICLE_NUMBER; i++) {
-				for (int j = 0; j < i; j++) {
-					d_mean += distance(p[j], p[i]);
-				}
-			}
-			d_mean *= (2. / (PARTICLE_NUMBER*(PARTICLE_NUMBER - 1)));
-			//cout << "d_mean: " << d_mean << "\n";
-			d_mean_mean += d_mean;
-		}
-		d_mean_mean *= 1. / number_of_sample[t];
+	
 
-		//cout << "number of sample: " << number_of_sample[t] << endl;
-		//cout << "mean of d_mean: " << d_mean_mean << endl;
-		myfile << number_of_sample[t] << "  " << d_mean_mean << "\n";
+	if (change_smapling_number) {
+		
+		string str = "len=" + to_string(CUBE_LENGTH) + " r=" + to_string(RADIUS) + " n=" \
+			+ to_string(PARTICLE_NUMBER) + " N_M=" + to_string(NUMBER_OF_NUMBER_OF_SAMPLE) + ".txt";
+		fstream myfile(str, ios::out | ios::trunc);
+		int number_of_sample[NUMBER_OF_NUMBER_OF_SAMPLE];
+		for (int i = 0; i < NUMBER_OF_NUMBER_OF_SAMPLE; i++) {
+			number_of_sample[i] = 1 * (i + 2);
+		}
+		particle *p;
+		p = new particle[PARTICLE_NUMBER];
+		srand48(time(NULL));
+		for (int t = 0; t < NUMBER_OF_NUMBER_OF_SAMPLE; t++) {
+			double d_mean_mean = 0;
+			for (int k = 0; k < number_of_sample[t]; k++) {
+				//generate a configration
+				for (int i = 0; i < PARTICLE_NUMBER; i++) {
+					int n = 0;
+					// propose a particle until it is accepted
+					do {
+						n++;
+						double p_newx, p_newy, p_newz;
+
+						p_newx = (CUBE_LENGTH - 1) * drand48();
+						//srand48(time(NULL));
+						p_newy = (CUBE_LENGTH - 1) * drand48();
+						//srand48(time(NULL));
+						p_newz = (CUBE_LENGTH - 1) * drand48();
+						particle p_new{ p_newx, p_newy, p_newz };
+						//cout << "p_new.x: " << p_new.x << " p_new.y: " << p_new.y << " p_new.z: " << p_new.z << "\n";
+						is_accepted = true;
+						for (int j = 0; j < i; j++) {
+							if (distance(p_new, p[j]) < 2 * RADIUS) {
+								is_accepted = false;
+								break;
+							}
+						}
+						if (is_accepted) {
+							p[i] = p_new;
+						}
+						if (n > 100) {
+							cout << "Cant place more particle\n";
+							volume *= PARTICLE_NUMBER;
+							cout << "volume: " << volume << "\n";
+							cout << "volume fraction: " << volume / pow(CUBE_LENGTH, 3) << "\n";
+							system("pause");
+							return 0;
+						}
+					} while (!is_accepted);
+				}
+				//calculate the mean of distance
+				double d_mean = 0;
+				for (int i = 0; i < PARTICLE_NUMBER; i++) {
+					for (int j = 0; j < i; j++) {
+						d_mean += distance(p[j], p[i]);
+					}
+				}
+				d_mean *= (2. / (PARTICLE_NUMBER*(PARTICLE_NUMBER - 1)));
+				//cout << "d_mean: " << d_mean << "\n";
+				d_mean_mean += d_mean;
+			}
+			d_mean_mean *= 1. / number_of_sample[t];
+
+			//cout << "number of sample: " << number_of_sample[t] << endl;
+			//cout << "mean of d_mean: " << d_mean_mean << endl;
+			myfile << number_of_sample[t] << "  " << d_mean_mean << "\n";
+		}
+		myfile.close();
+		delete[] p;
 	}
-	myfile.close();
+
+	if (change_particle_number) {
+		string str = "len=" + to_string(CUBE_LENGTH) + " r=" + to_string(RADIUS) + " N_n=" \
+			+ to_string(NUMBER_OF_PARTICLE_NUMBER) + " M=" + to_string(NUMBER_OF_SAMPLE) + ".txt";
+		fstream myfile(str, ios::out | ios::trunc);
+		int number_of_particle[NUMBER_OF_PARTICLE_NUMBER];
+		for (int i = 0; i < NUMBER_OF_PARTICLE_NUMBER; i++) {
+			number_of_particle[i] = 1 * (i + 2);
+		}
+		
+		srand48(time(NULL));
+		for (int t = 0; t < NUMBER_OF_PARTICLE_NUMBER; t++) {
+			double d_mean_mean = 0;
+			particle *p;
+			p = new particle[number_of_particle[t]];
+			for (int k = 0; k < NUMBER_OF_SAMPLE; k++) {
+				//generate a configration
+				for (int i = 0; i < number_of_particle[t]; i++) {
+					int n = 0;
+					// propose a particle until it is accepted
+					do {
+						n++;
+						double p_newx, p_newy, p_newz;
+
+						p_newx = (CUBE_LENGTH - 1) * drand48();
+						//srand48(time(NULL));
+						p_newy = (CUBE_LENGTH - 1) * drand48();
+						//srand48(time(NULL));
+						p_newz = (CUBE_LENGTH - 1) * drand48();
+						particle p_new{ p_newx, p_newy, p_newz };
+						//cout << "p_new.x: " << p_new.x << " p_new.y: " << p_new.y << " p_new.z: " << p_new.z << "\n";
+						is_accepted = true;
+						for (int j = 0; j < i; j++) {
+							if (distance(p_new, p[j]) < 2 * RADIUS) {
+								is_accepted = false;
+								break;
+							}
+						}
+						if (is_accepted) {
+							p[i] = p_new;
+						}
+						if (n > 100) {
+							cout << "Cant place more particle\n";
+							volume *= number_of_particle[t];
+							cout << "volume: " << volume << "\n";
+							cout << "volume fraction: " << volume / pow(CUBE_LENGTH, 3) << "\n";
+							system("pause");
+							return 0;
+						}
+					} while (!is_accepted);
+				}
+				//calculate the mean of distance
+				double d_mean = 0;
+				for (int i = 0; i < number_of_particle[t]; i++) {
+					for (int j = 0; j < i; j++) {
+						d_mean += distance(p[j], p[i]);
+					}
+				}
+				d_mean *= (2. / (number_of_particle[t]*(number_of_particle[t] - 1)));
+				//cout << "d_mean: " << d_mean << "\n";
+				d_mean_mean += d_mean;
+			}
+			d_mean_mean *= 1. / NUMBER_OF_SAMPLE;
+
+			//cout << "number of sample: " << number_of_sample[t] << endl;
+			//cout << "mean of d_mean: " << d_mean_mean << endl;
+			myfile << number_of_particle[t] << "  " << d_mean_mean << "\n";
+			delete[] p;
+		}
+		myfile.close();
+	}
+	
 
 	//system("pause");
 	return 0;
